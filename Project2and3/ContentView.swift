187250ba566ecc4ed30ef2abc60a95a07f92a646
ZackOBrien <obrienzj19@gcc.edu>
2022-11-11 11:31:19 -0500

    Initial Commit

---
+++ Project2and3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project2and3
+//
+//  Created by Zack O'Brien on 11/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project2and3/Project2and3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2and3App.swift
+//  Project2and3
+//
+//  Created by Zack O'Brien on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Project2and3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
