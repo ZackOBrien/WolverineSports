da3c55d04d25bea4c06fbc3a2228027c00b0657e
ZackOBrien <obrienzj19@gcc.edu>
2022-11-13 17:19:56 -0500

    fixes made to the profile view

--- Project2and3/ProfileView.swift
+++ Project2and3/ProfileView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
+        NavigationView{
         VStack{
             HStack{
                 Image(systemName: "person.crop.circle.fill")
@@ -16,10 +17,22 @@ struct ProfileView: View {
                     .frame(width: 75, height: 75)
                     .foregroundColor(.red)
                     .padding()
-                Text("Chris Gerello")
-                    .font(.title2)
-                    .bold()
+                VStack{
+                    Text("Chris Gerello")
+                        .font(.title2)
+                        .bold()
+                    Text("Senior 23'")
+                    Text("Computer Science")
+                    Text("Number of Teams: 10")
+                }
             }
+            Spacer()
+            Text("Biography:")
+                .font(.title2)
+                .bold()
+                .frame(maxWidth:.infinity, alignment: .leading)
+            Text("Hello I am Chris Gerello, I am a senior Computer Science major. I enjoy to play in intermural sports with my friends and having this app has sure streamlined the process of signing-up, finding a team, and playing games.")
+                .foregroundColor(.black)
             Text("Your Teams:")
                 .font(.title2)
                 .bold()
@@ -27,17 +40,31 @@ struct ProfileView: View {
             
             ScrollView(.horizontal) {
                 HStack(spacing: 20) {
-                    ForEach(0..<10) {
-                        Text("Item \($0)")
+                    ForEach(0..<10) {_ in
+                        NavigationLink{
+                            TeamView()
+                        } label: {
+                            HStack{
+                                Image(systemName: "circle")
+                                    .resizable()
+                                    .frame(width: 25, height: 25)
+                                    .foregroundColor(.white)
+                                Text("Team")
+                                    .font(.title2)
+                            }
                             .foregroundColor(.white)
-                            .font(.largeTitle)
-                            .frame(width: 200, height: 150)
+                            .frame(width: 150, height: 100)
                             .background(.red)
                             .cornerRadius(20)
+                        }
                     }
                 }
             }
-            
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+        }
         }
     }
 }
